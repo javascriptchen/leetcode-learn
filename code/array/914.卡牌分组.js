6d7909555bb2921e9608080c2f7761462737826f
chenchen <458679677@qq.com>
2019-04-08 11:43:37 +0800

    914未完成

--- code/array/914.卡牌分组.js
+++ code/array/914.卡牌分组.js
@@ -1,30 +1,41 @@
 export default (deck) => {
-    deck.sort((a, b) => a - b);
-    let min = Number.MAX_SAFE_INTEGER;
-    let dst = [];
-    let result = true;
-    for (let i = 0, len = deck.length, tmp = []; i < len; i++) {
-        tmp.push(deck[i]);
-        for (let j = i + 1, len = deck.length; j < len - 1; j++) {
-            if (deck[i] === deck[j]) {
-                tmp.push(deck[j]);
-            } else {
-                if (min > tmp.length) {
-                    min = tmp.length;
-                }
-                // tmp是引用类型变量
-                dst.push([].concat(tmp));
-                tmp.length = 0;
-                i = j;
-                break;
-            }
+  deck.sort((a, b) => a - b);
+  let min = Number.MAX_SAFE_INTEGER;
+  let dst = [];
+  let result = true;
+  if (deck.length < 2) {
+    result = false;
+    return false;
+  }
+  for (let i = 0, len = deck.length, tmp = []; i < len; i++) {
+    tmp.push(deck[i]);
+    for (let j = i + 1; j < len + 1; j++) {
+      if (deck[i] === deck[j]) {
+        tmp.push(deck[j]);
+      } else {
+        if (min > tmp.length) {
+          min = tmp.length;
         }
+        // tmp是引用类型变量
+        dst.push([].concat(tmp));
+        tmp.length = 0;
+        i = j - 1;
+        break;
+      }
     }
-    dst.every(item => {
-        if (item.length % min !== 0) {
-            result = false;
-            return false;
-        }
-    });
-    return result;
+  }
+  // console.log(dst);
+  dst.every(item => {
+    // if (min === 1) {
+    //     result = false;
+    //     return false;
+    // }
+    console.log(item.length);
+    console.log(min);
+    if (item.length % min !== 0 || min < 2) {
+      result = false;
+      return false;
+    }
+  });
+  return result;
 };
